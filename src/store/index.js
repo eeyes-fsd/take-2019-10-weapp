cfa5ab63baddceadec0f7c210a4193af939ae267
mhq1065 <mhq1065@qq.com>
2019-10-05 21:21:42 +0800

    假装写好了正则

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,50 @@ import Vuex from '@wepy/x'
 
 export default new Vuex.Store({
   state: {
-    counter: 0
+    counter: 0,
+    goodslist: [
+      {
+        name: '菠萝',
+        id: '123',
+        quantity: 0
+      },
+      {
+        name: '苹果',
+        id: '12345',
+        quantity: 0
+      },
+      {
+        name: '里头',
+        id: '1245',
+        quantity: 0
+      },
+      {
+        name: '桃子',
+        id: '2345',
+        quantity: 0
+      },
+      {
+        name: 'peach',
+        id: '23452',
+        quantity: 0
+      },
+      {
+        name: 'liulian',
+        id: '23945',
+        quantity: 0
+      }
+    ]
   },
   mutations: {
-    increment(state) {
-      state.counter++
+    increment(state, key) {
+      state.goodslist.filter(x => {
+        return x.id === key
+      })[0].quantity++
     },
-    decrement(state) {
-      state.counter--
+    decrement(state, key) {
+      state.goodslist.filter(x => {
+        return x.id === key
+      })[0].quantity--
     }
   },
   actions: {
