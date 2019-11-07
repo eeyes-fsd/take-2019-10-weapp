d73fc221c73b10861bab9f561ae034575a48381c
mhq1065 <mhq1065@qq.com>
2019-11-07 21:34:44 +0800

    订单查询

--- src/utils/order.js
+++ src/utils/order.js
@@ -20,39 +20,71 @@ const getpayment = async () => {
     return res
 }
 const pay = async () => {
-    let payment = await getpayment().data
+    let payment = (await getpayment()).data
     if (!payment) {
         return
     }
-    wx.requestPayment(
-        {
-            'timeStamp': payment.timeStamp,
-            'nonceStr': payment.nonceStr,
-            'package': payment.package,
-            'signType': 'MD5',
-            'paySign': payment.paySign,
-            'success': function (res) {
-                console.log(res)
-                wx.showToast({
-                    title: '支付成功',
-                    icon: 'success',
-                    duration: 2000
-                })
-                wx.navigateTo({
-                    url: '/pages/paysuccess'
-                });
-            },
-            'fail': function (res) {
-                console.log(res)
-                wx.showToast({
-                    title: '支付失败',
-                    icon: 'none',
-                    duration: 1200
-                })
-            },
-            'complete': function (res) { }
-        })
+    wx.requestPayment({
+        'timeStamp': payment.timeStamp,
+        'nonceStr': payment.nonceStr,
+        'package': payment.package,
+        'signType': 'MD5',
+        'paySign': payment.paySign,
+        'success': function (res) {
+            console.log(res)
+            wx.showToast({
+                title: '支付成功',
+                icon: 'success',
+                duration: 2000
+            })
+            wx.navigateTo({
+                url: '/pages/paysuccess'
+            });
+        },
+        'fail': function (res) {
+            console.log(res)
+            wx.showToast({
+                title: '支付失败',
+                icon: 'none',
+                duration: 1200
+            })
+        }
+        // 'complete': function (res) { }
+    })
 }
+
+const getOrders = async (i) => {
+    let token = await getToken()
+    let res = await wepy.promisify(wx.request)({
+        url: (i === undefined) ? `${api_host}/orders` : `${api_host}/orders/${i}`,
+        method: "GET",
+        header: { 'Authorization': `Bearer ${token}` }
+    })
+    return res
+}
+const cancelOrder = async (i) => {
+    let token = await getToken()
+    let res = await wepy.promisify(wx.request)({
+        url: `${api_host}/orders/${i}`,
+        method: "DELETE",
+        header: { 'Authorization': `Bearer ${token}` }
+    })
+    return res
+}
+const setupOrder = async (details, address_id) => {
+    let token = await getToken()
+    let res = await wepy.promisify(wx.request)({
+        url: `${api_host}/orders/`,
+        method: "POST",
+        header: { 'Authorization': `Bearer ${token}` },
+        data: {
+            "details": details,
+            "address_id": address_id
+        }
+    })
+    return res
+}
+export { getOrders, cancelOrder, setupOrder }
 export default {
     pay
 }
